777bb27020a006b2ef6c6895f6c61922560cdb86
andrey.plakhotin <andrey.plakhotin@softswiss.com>
2024-08-06 21:19:42 +0300

    init project

---
+++ src/main/java/ru/tms/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package ru.tms.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .cors(Customizer.withDefaults())
+                .sessionManagement((sessionManagement) ->
+                        sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests((authorizeHttpRequests) ->
+                        authorizeHttpRequests.requestMatchers("/api/auth/**").permitAll()
+                                .requestMatchers("/demo").permitAll()
+                                .anyRequest().authenticated())
+                .build();
+    }
+}
---
+++ src/main/java/ru/tms/controller/IndexController.java
@@ -0,0 +1,15 @@
+package ru.tms.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping("demo")
+    public String demo() {
+        return "demo";
+    }
+}
